f0d36604a9498a14173cfc0f7bfb757b221398a6
Nathan Breit <nabreit@gmail.com>
2012-08-24 23:03:49 -0400

    fixed fiducial issue

--- static/formDrawer/formDrawer.js
+++ static/formDrawer/formDrawer.js
@@ -30,7 +30,6 @@ function computeMarkupLocation(field) {
 
 $('.viewAsImage').click(function() {
     //Create a modal
-    $('body').append("<img src='" + uriContent + "' style='display: none;' ></img>");
     var $body = $('.modal-body');
     $body.empty();
     var uriContent = $("canvas").getCanvasImage("jpeg", 1.0);
@@ -58,8 +57,98 @@ function createForm(form) {
         height: form.height,
         width: form.width
     });
+    //$canvas.drawLayers();
 
-    var fiducials = [{
+    var title = $canvas.addLayer({
+        x: form.width / 2,
+        y: 50,
+        method: "drawText",
+        fillStyle: "#000",
+        align: "center",
+        baseline: "middle",
+        font: "12pt Verdana, sans-serif",
+        text: form.form_title
+    });
+
+    ///////Draw form:
+    $.each(form.fields, function(field_idx, field) {
+        field = $.extend({}, form, field);
+        progress += 90 / numFields;
+        $bar.css('width', progress + '%');
+        
+        var markup_object = {
+            method: "drawText",
+            fillStyle: "#000",
+            opacity: 0.7,
+            x: 0,
+            y: 0,
+            align: "left",
+            baseline: "top",
+            font: "12pt Verdana, sans-serif",
+            text: field.label || field.name
+        };
+        $.extend(markup_object, computeMarkupLocation(field));
+        if ("markup_location" in field) {
+            $.extend(markup_object, field.markup_location);
+        }
+
+        var textLayer = $canvas.addLayer(markup_object);
+        
+        $.each(field.segments, function(segment_idx, segment) {
+            var classifier;
+            segment = $.extend({}, field, segment);
+            classifier = segment.classifier;
+            $canvas.addLayer({
+                method: 'drawRect',
+                strokeStyle: "#000000",
+                strokeWidth: 2,
+                fromCenter: false,
+                x: segment.segment_x,
+                y: segment.segment_y,
+                width: segment.segment_width,
+                height: segment.segment_height
+            });
+            if ('items' in segment) {
+                $.each(segment.items, function(item_idx, item) {
+                    $canvas.addLayer({
+                        method: classifier.training_data_uri === "bubbles" ? "drawEllipse" : "drawRect",
+                        strokeStyle: "#55d",
+                        strokeWidth: 2,
+                        fromCenter: true,
+                        name: "myBox",
+                        group: "myBoxes",
+                        x: segment.segment_x + item.item_x,
+                        y: segment.segment_y + item.item_y,
+                        width: classifier.classifier_width * 0.8,
+                        height: classifier.classifier_height * 0.8
+                    });
+                    if ('label' in item) {
+                        var itemLabelObj = {
+                            method: "drawText",
+                            fillStyle: "#000",
+                            opacity: 0.7,
+                            x: segment.segment_x + item.item_x - classifier.classifier_width,
+                            y: segment.segment_y + item.item_y,
+                            align: "right",
+                            font: "12pt Verdana, sans-serif",
+                            text: item.label
+                        };
+                        $canvas.addLayer(itemLabelObj);
+                    }
+                });
+            }
+            else {
+
+            }
+        });
+
+    });$canvas.drawLayers();
+    if (progress == 100) {
+        window.setTimeout(function() {
+            $bar.parent().remove();
+        }, 1000);
+    }
+        var fiducials = [{
         source: "fiducials/cs.jpg",
         x: 50,
         y: 40
@@ -87,102 +176,13 @@ function createForm(form) {
             fromCenter: true //I think this has to be true if there is no height/width
         }, fiducial));
     });
-    
-    var title = $canvas.addLayer({
-        x: form.width/2,
-        y: 50,
-        method: "drawText",
-        fillStyle: "#000",
-        align: "center",
-        baseline: "middle",
-        font: "12pt Verdana, sans-serif",
-        text: form.form_title
-    });
-    
-    ///////Draw form:
-    $.each(form.fields, function(field_idx, field) {
-        field = $.extend({}, form, field);
-        progress += 90 / numFields;
-        $bar.css('width', progress + '%');
-
-        var markup_object = {
-            method: "drawText",
-            fillStyle: "#000",
-            opacity: 0.7,
-            x: 0,
-            y: 0,
-            align: "left",
-            baseline: "top",
-            font: "12pt Verdana, sans-serif",
-            text: field.label || field.name
-        };
-        $.extend(markup_object, computeMarkupLocation(field));
-        if ("markup_location" in field) {
-            $.extend(markup_object, field.markup_location);
-        }
-
-        var textLayer = $canvas.addLayer(markup_object);
-
-        $.each(field.segments, function(segment_idx, segment) {
-            var classifier;
-            segment = $.extend({}, field, segment);
-            classifier = segment.classifier;
-            $canvas.addLayer({
-                method: 'drawRect',
-                strokeStyle: "#000000",
-                strokeWidth: 2,
-                fromCenter: false,
-                x: segment.segment_x,
-                y: segment.segment_y,
-                width: segment.segment_width,
-                height: segment.segment_height
-            });
-            if('items' in segment){
-                $.each(segment.items, function(item_idx, item) {
-                    $canvas.addLayer({
-                        method: classifier.training_data_uri === "bubbles" ? "drawEllipse" : "drawRect",
-                        strokeStyle: "#55d",
-                        strokeWidth: 2,
-                        fromCenter: true,
-                        name: "myBox",
-                        group: "myBoxes",
-                        x: segment.segment_x + item.item_x,
-                        y: segment.segment_y + item.item_y,
-                        width: classifier.classifier_width * .8,
-                        height: classifier.classifier_height * .8
-                    });
-                    if('label' in item) {
-                        var itemLabelObj = {
-                            method: "drawText",
-                            fillStyle: "#000",
-                            opacity: 0.7,
-                            x: segment.segment_x + item.item_x - classifier.classifier_width,
-                            y: segment.segment_y + item.item_y,
-                            align: "right",
-                            font: "12pt Verdana, sans-serif",
-                            text: item.label
-                        };
-                        $canvas.addLayer(itemLabelObj);
-                    }
-                });
-            } else {
-                
-            }
-        });
-        if (progress == 100) {
-            window.setTimeout(function() {
-                $bar.parent().remove();
-            }, 1000);
-        }
-    });
     var fiducialsLoaded = 0;
-    function fiducialLoaded(){
-    	fiducialsLoaded++;
-    	console.log('fiducials loaded: ' + fiducialsLoaded)
-    	if(fiducialsLoaded === 4){
-		    $canvas.drawLayers();
-		    $('.viewAsImage').click();
-    	}
+    function fiducialLoaded(a,b,c) {
+        console.log('fiducials loaded');
+        fiducialsLoaded++;
+        if (fiducialsLoaded === 4) {
+            $('.viewAsImage').click();
+        }
     }
 
 }
